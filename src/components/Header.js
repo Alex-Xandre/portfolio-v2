58b5918f004cd23a7af6a1b1e735a3a52740d7c2
Alex-Xandre <96884541+Alex-Xandre@users.noreply.github.com>
2022-10-08 23:44:35 +0800

    sevices b-radius

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,7 +39,7 @@ const Header = ({ theme }) => {
                 mode: "push",
               },
               onHover: {
-                enable: true,
+                enable: false,
                 mode: "repulse",
               },
               resize: true,
