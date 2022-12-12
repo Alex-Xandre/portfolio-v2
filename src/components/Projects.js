e9eed043197e051ba4c625cef87d1fefe76705da
Seldom Madallang <micua.161472@vigan.sti.edu.ph>
2022-12-12 15:28:58 +0800

    projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,27 +9,29 @@ const Projects = () => {
     Aos.refresh();
   }, []);
 
+  console.log(Project)
+
   return (
     <section className="project_section" data-aos="fade-up">
       <h1>
         <span>&#60;</span> My Projects <span> &#47;&#62;</span>
       </h1>
-      <div class="star"></div>
-      <div class="meteor-1"></div>
-      <div class="meteor-2"></div>
-      <div class="meteor-3"></div>
-      <div class="meteor-4"></div>
-      <div class="meteor-5"></div>
-      <div class="meteor-6"></div>
-      <div class="meteor-7"></div>
-      <div class="meteor-8"></div>
-      <div class="meteor-9"></div>
-      <div class="meteor-10"></div>
-      <div class="meteor-11"></div>
-      <div class="meteor-12"></div>
-      <div class="meteor-13"></div>
-      <div class="meteor-14"></div>
-      <div class="meteor-15"></div>
+      <div className="star"></div>
+      <div className="meteor-1"></div>
+      <div className="meteor-2"></div>
+      <div className="meteor-3"></div>
+      <div className="meteor-4"></div>
+      <div className="meteor-5"></div>
+      <div className="meteor-6"></div>
+      <div className="meteor-7"></div>
+      <div className="meteor-8"></div>
+      <div className="meteor-9"></div>
+      <div className="meteor-10"></div>
+      <div className="meteor-11"></div>
+      <div className="meteor-12"></div>
+      <div className="meteor-13"></div>
+      <div className="meteor-14"></div>
+      <div className="meteor-15"></div>
 
       {Project.map((proj, index) => {
         return (
